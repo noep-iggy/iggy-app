81b873146308b3837af4c55805be7de8f22bc0a3
Dorian Longépée <dorianlongepee.pro@gmail.com>
2023-10-26 21:15:07 +0200

    🔧 Config Prettier + Support TW + Architecture projet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from 'nativewind';
+
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! coucou</Text>
-      <StatusBar style='auto' />
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Text className="dark:text-white">
+        Open up App.tsx to start working on your app! 😀
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
