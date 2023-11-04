e7c854faad224398fb03b24353ffcf946e4dba85
Dorian Longépée <dorianlongepee.pro@gmail.com>
2023-11-04 14:50:21 +0100

    🗺️ Set up expo router & remove nativewind

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function Layout() {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app! 😀</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
